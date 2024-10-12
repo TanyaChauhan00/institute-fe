524dc6e39ca259cf480d12f747e441b1cb1a49d4
TanyaChauhan00 <tanuuchauhan2000@gmail.com>
2024-10-12 23:06:53 +0530

    feat:new changes ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HostListener } from '@angular/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,4 +11,45 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'techglide-test';
+  @HostListener('document:contextmenu', ['$event'])
+  disableRightClick(event: MouseEvent): void {
+    event.preventDefault();
+  }
+
+  // Disable copy, cut, and paste globally
+  @HostListener('document:copy', ['$event'])
+  disableCopy(event: ClipboardEvent): void {
+    event.preventDefault();
+  }
+
+  @HostListener('document:cut', ['$event'])
+  disableCut(event: ClipboardEvent): void {
+    event.preventDefault();
+  }
+
+  @HostListener('document:paste', ['$event'])
+  disablePaste(event: ClipboardEvent): void {
+    event.preventDefault();
+  }
+
+  // Disable certain key combinations (F12, Ctrl+Shift+I/J, Ctrl+U)
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    // Prevent F12
+    if (event.key === 'F12') {
+      event.preventDefault();
+    }
+    // Prevent Ctrl+Shift+I (Chrome Dev Tools)
+    if (event.ctrlKey && event.shiftKey && event.key === 'I') {
+      event.preventDefault();
+    }
+    // Prevent Ctrl+Shift+J (Console)
+    if (event.ctrlKey && event.shiftKey && event.key === 'J') {
+      event.preventDefault();
+    }
+    // Prevent Ctrl+U (View Source)
+    if (event.ctrlKey && event.key === 'U') {
+      event.preventDefault();
+    }
+  }
 }
